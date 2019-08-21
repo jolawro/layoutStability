71f667a80f288693ac8452328984fb3ad9a69886
Ksymena Basińska <ksymena.basinska@miquido.com>
2019-08-21 22:33:13 +0200

    Tabula rasa

--- src/hooks/useLayoutObserver.js
+++ src/hooks/useLayoutObserver.js
@@ -1,23 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 
 export function useLayoutObserver() {
-  const [entries, updateEntries] = useState([]);
 
-  const observer = useRef(
-    new PerformanceObserver(list => {
-      list.getEntries().map(console.log)
-      const newEntries = list.getEntries().map(entry => entry.value)
-      updateEntries([...entries, ...newEntries]);
-    })
-  );
-
-  useEffect(() => {
-    const { current: currentObserver } = observer;
-
-    currentObserver.observe({ type: "layoutShift", buffered: true });
-
-    return () => currentObserver.disconnect();
-  }, []);
-
-  return entries;
+  return [];
 }
